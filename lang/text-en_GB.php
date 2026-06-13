56c45d52031bac4f768a0397b0220bcc3fb87bac
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-13 15:10:27 +0300

    Файл локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения RosGear.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    '{name}'        => 'Article',
+    '{description}' => 'Site article',
+
+    // Widget: разметка
+    'Site article "{0}"' => 'Site article - {0}',
+    'All articles' => 'All articles',
+    'Markup settings' => 'Markup settings',
+    'Edit article' => 'Edit article',
+    'Add article' => 'Add article',
+    'Delete article' => 'Delete article',
+    // Widget: разметка / сообщения
+    'Are you sure you want to delete the article?' => 'Are you sure you want to delete the article?',
+
+    // MarkupSettings
+    '{markupsettings.title}' => 'Widget markup settings "Article"',
+    // MarkupSettings: поля
+    'Template markup' => 'Template markup',
+    'In the specified template, the widget parameters are changed. You can make changes manually by opening the template for editing.' 
+        => 'In the specified template, the widget parameters are changed. You can make changes manually by opening the template for editing.',
+    'Show header' => 'Show header',
+    'Show announcement image' => 'Show announcement image',
+    'Show content (components) before article text' => 'Show content (components) before article text',
+    'Show content (components) after the article text' => 'Show content (components) after the article text',
+    'Show publication date' => 'Show publication dat'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения RosGear.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    '{name}'        => 'Материал',
+    '{description}' => 'Материал сайта',
+
+    // Widget: разметка
+    'Site article "{0}"' => 'Материал - {0}',
+    'All articles' => 'Все материалы',
+    'Markup settings' => 'Настройка разметки',
+    'Edit article' => 'Редактировать материал',
+    'Add article' => 'Добавить материал',
+    'Delete article' => 'Удалить материал',
+    // Widget: разметка / сообщения
+    'Are you sure you want to delete the article?' => 'Вы действительно хотите удалить материал?',
+
+    // MarkupSettings
+    '{markupsettings.title}' => 'Настройка разметки виджета "Материал"',
+    // MarkupSettings: поля
+    'Template markup' => 'Разметка в шаблоне',
+    'In the specified template, the widget parameters are changed. You can make changes manually by opening the template for editing.' 
+        => 'В указанном шаблоне изменяются параметры виджета. Изменения вы можете сделать вручную, открыв на редактирование шаблон.',
+    'Show header' => 'Показать заголовок',
+    'Show announcement image' => 'Показать изображение анонса в материале',
+    'Show content (components) before article text' => 'Показать содержимое (компонентов) перед текстом материала',
+    'Show content (components) after the article text' => 'Показать содержимое (компонентов) после текста материала',
+    'Show publication date' => 'Показать дату публикации'
+];
